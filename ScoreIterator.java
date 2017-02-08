21cd35b5899d1379e00d168b7adc94cd43565976
yuchenbai98 <yuchenbai1998@yahoo.com>
2017-02-07 20:24:22 -0600

    Update ScoreIterator.java

--- ScoreIterator.java
+++ ScoreIterator.java
@@ -1,4 +1,24 @@
+import java.util.NoSuchElementException;
 
-public class ScoreIterator {
+public class ScoreIterator<E> implements ScoreIteratorADT<Score> {
+	private ScoreList myLists;
+	private int currItems;
+
+	public ScoreIterator (ScoreList myLists){
+		this.myLists = myLists;
+		this.currItems = 0;
+	}
+	@Override
+	public boolean hasNext() {
+		return currItems< myLists.size();
+	}
+
+	@Override
+	public Score next() {
+		if (!hasNext()) throw new NoSuchElementException();
+		Score result = myLists.get(currItems);
+		currItems++;
+		return result;
+	}
 
 }
