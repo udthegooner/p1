d7cbc08b96d1dc16b56fe970cdd1ecc9603ffe27
Quinnbreitnicholson <quinnbreitnicholson@gmail.com>
2017-02-07 19:51:36 -0600

    Update GradeFileFormatException.java

--- GradeFileFormatException.java
+++ GradeFileFormatException.java
@@ -1,4 +1,15 @@
+/**
+ * This class is a checked exception to be thrown when the 
+ * input file to the GradeEstimator is improperly formatted.
+ * 
+ * <p> bugs: None known.
+ * 
+ * @author Quinn Breit-Nicholso
+ *
+ */
 
-public class GradeFileFormatException {
-
+public class GradeFileFormatException extends Exception{
+	
+	public GradeFileFormatException() {} // an empty constructor
+	
 }
