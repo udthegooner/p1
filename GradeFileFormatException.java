ad1e47f51fe01c37979def4e5d61edb1e174d1a7
Collin-Lacy <clacy@wisc.edu>
2017-02-09 22:01:06 -0600

    Update GradeFileFormatException.java

--- GradeFileFormatException.java
+++ GradeFileFormatException.java
@@ -1,15 +1,60 @@
-/**
- * This class is a checked exception to be thrown when the 
- * input file to the GradeEstimator is improperly formatted.
- * 
- * <p> bugs: None known.
- * 
- * @author Quinn Breit-Nicholso
- *
- */
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017
+// PROJECT:          p1
+// FILE:             GradeFileFormatException.java
+//
+// Authors: Collin Lacy, clacy@wisc.edu, Lecture 001
+// Author1: (name1,email1,netID1,lecture number1)
+// Author2: (name2,email2,netID2,lecture number2)
+//
+// ---------------- OTHER ASSISTANCE CREDITS 
+// Persons: Identify persons by name, relationship to you, and email. 
+// Describe in detail the the ideas and help they provided. 
+// 
+// Online sources: avoid web searches to solve your problems, but if you do 
+// search, be sure to include Web URLs and description of 
+// of any information you find. 
+//////////////////////////// 80 columns wide //////////////////////////////////
 
-public class GradeFileFormatException extends Exception{
+
+
+
+/**
+ * This class is responsible for handling a custom, checked exception. 
+ * 
+ * To be thrown when the input file to
+ * the GradeEstimator is improperly formatted.
+ *
+ *
+ * @author Lacy
+ */
+public class GradeFileFormatException extends Exception {
 	
-	public GradeFileFormatException() {} // an empty constructor
+	/**
+	 * 
+	 * If the constructor does not explicitly call super() with
+	 * or without some arguments, java will automatically call the 
+	 * default constructor of the superclass. 
+	 * 
+	 * This is just a reminder that the base class objects
+	 * must be built as part of the sub class object.
+	 *
+	 */
+	public GradeFileFormatException() {
+		super();
+	}
+	
+	/**
+         * Uses super to initialize the Exception's error message.
+	 * By passing the message into the base class constructor, the base
+	 * class will take care of the work of setting up the message correctly.
+	 * 
+	 *
+	 * @param message	String that describes the error condition for this particular exception
+	 * 
+	 */
+	public GradeFileFormatException(String message) {
+		super(message);
+	}
 	
 }
