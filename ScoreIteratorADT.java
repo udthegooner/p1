79780f119951fbc4217353190fa86535b14dcda8
yuchenbai98 <yuchenbai1998@yahoo.com>
2017-02-12 19:05:22 -0600

    Update ScoreIteratorADT.java

--- ScoreIteratorADT.java
+++ ScoreIteratorADT.java
@@ -1,4 +1,17 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017
+// PROJECT:          p1
+// FILE:             ScoreIteratorADT.java
+//
+// Authors: Yuchen Bai, bai45@wisc.edu, Lecture 002
+//////////////////////////// 80 columns wide //////////////////////////////////
 
+/**
+ * This class is ScoreIteratorADT
+ *
+ *
+ * @author Yuchen Bai
+ */
 public interface ScoreIteratorADT <E> {
 	public boolean hasNext();
 	public E next();
